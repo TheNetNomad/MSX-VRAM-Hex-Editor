43cfa67907d8def95ab03ebb94e26219d3b0522c
_NetNomad <43683711+TheNetNomad@users.noreply.github.com>
2024-02-19 21:34:34 -0500

    Create Converter.js

---
+++ Converter.js
@@ -0,0 +1,83 @@
+// classes
+ 
+function Converter()
+{
+	// static class
+}
+
+{
+	Converter.AsciiCharacters =
+		"â€â˜ºâ˜»â™¥â™¦â™£â™ â€¢â—˜â—‹â—™â™‚â™€â™ªâ™«â˜¼â–º" // 0-16
+		+ "â—„â†•â€¼Â¶Â§â–¬â†¨â†‘â†“â†’â†âˆŸâ†”â–²â–¼â " // 17-32
+		+ "!\"#$%&'()*+,-./0" // 33-48
+		+ "123456789:;<=>?@" // 49-64
+		+ "ABCDEFGHIJKLMNOP" // 65-80
+		+ "QRSTUVWXYZ[\\]^_`" // 81-96
+		+ "abcdefghijklmnop" // 97-112
+		+ "qrstuvwxyz{|}~âŒ‚Ã‡" // 113-128
+		+ "Ã¼Ã©Ã¢Ã¤Ã Ã¥Ã§ÃªÃ«Ã¨Ã¯Ã®Ã¬Ã„Ã…Ã‰" // 129-144
+		+ "Ã¦Ã†Ã´Ã¶Ã²Ã»Ã¹Ã¿Ã–ÃœÂ¢Â£Â¥â‚§Æ’Ã¡" // 145-160
+		+ "Ã³ÃºÃ±Ã‘ÂªÂºÂ¿âŒÂ¬Â½Â¼Â¡Â«Â»â–‘â–’" // 161-176
+		+ "â–’â–“â”‚â”¤â•¡â•¢â•–â••â•£â•‘â•—â•â•œâ•›â”â””" // 177-192
+		+ "â”´â”¬â”œâ”€â”¼â•žâ•Ÿâ•šâ•”â•©â•¦â• â•â•¬â•§â•¨" // 193-208
+		+ "â•¤â•¥â•™â•˜â•’â•“â•«â•ªâ”˜Ã‡â–ˆâ–„â–Œâ–â–€Î±" // 209-224
+		+ "ÃŸÎ“Ï€Î£ÏƒÂµÏ„Î¦Î˜Î©Î´âˆžÏ†Îµâˆ©â‰¡" // 225-240
+		+ "Â±â‰¥â‰¤âŒ âŒ¡Ã·â‰ˆÂ°âˆ™Â·âˆšâ¿Â²â– â½"; //241-255Â 
+ 
+	Converter.bytesToStringASCII = function(bytes)
+	{
+		var returnValue = "";
+ 
+		for (var i = 0; i < bytes.length; i++)
+		{
+			var byte = bytes[i];
+			var byteAsCharASCII = String.fromCharCode(Converter.AsciiCharacters.codePointAt(byte)); 
+			returnValue += byteAsCharASCII;
+		}
+ 
+		return returnValue;
+	}
+ 
+	Converter.bytesToStringHexadecimal = function(bytes)
+	{
+		var returnValue = "";
+ 
+		for (var i = 0; i < bytes.length; i++)
+		{
+			var byte = bytes[i];
+			var byteAsStringHexadecimal = 
+				byte.toString(16).padLeft(2, '0');
+ 
+			returnValue += byteAsStringHexadecimal;
+		}
+ 
+		return returnValue;
+	}
+ 
+	Converter.stringHexadecimalToBytes = function(stringHexadecimal)
+	{
+		var returnValues = [];
+ 
+		var nibblesForByteCurrent = [];
+ 
+		for (i = 0; i < stringHexadecimal.length; i++)
+		{
+			var charForNibble = stringHexadecimal[i];
+			var nibbleAsInt = parseInt(charForNibble, 16);
+			if (isNaN(nibbleAsInt) == false)
+			{
+				nibblesForByteCurrent.push(nibbleAsInt);
+				if (nibblesForByteCurrent.length == 2)
+				{
+					var byte = 
+						(nibblesForByteCurrent[0] << 4) 
+						+ nibblesForByteCurrent[1];
+					returnValues.push(byte);
+					nibblesForByteCurrent.length = 0;
+				}
+			}		   
+		}
+ 
+		return returnValues;
+	}   
+}
